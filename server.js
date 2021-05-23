5660503fd84fc7efe1f547cdc720dfc32f4f91d9
dshaw1242 <david.shaw1242@gmail.com>
2021-05-23 13:42:05 -0600

    added mysql2 db connection

--- server.js
+++ server.js
@@ -2,11 +2,23 @@ const e = require('express');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = mysql.createConnection(
+    {
+        host:'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // SQL Password
+        password: 'SurfaceSkis12!!',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
 
 app.get('/', (req, res) => {
     res.json({
