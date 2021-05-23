569c9fab2cca3a141b6de1e8ff369a075df862d9
dshaw1242 <david.shaw1242@gmail.com>
2021-05-23 13:57:10 -0600

    setup delete, create, and select single and all candidate queries

--- server.js
+++ server.js
@@ -26,10 +26,41 @@ app.get('/', (req, res) => {
     });
 });
 
+// Query to pull all candidate information
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// Delete a Candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
 });
+
+
+
 
 app.use((req, res) => {
     res.status(404).end();
