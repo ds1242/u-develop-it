5d3db6b5bf9aa2d1dccba4bab068a8e572821f7a
dshaw1242 <david.shaw1242@gmail.com>
2021-05-25 06:49:20 -0600

    cleaned up server.js organization

--- server.js
+++ server.js
@@ -1,32 +1,25 @@
-const e = require('express');
 const express = require('express');
-const { rest } = require('lodash');
+const db = require('./db/connection');
+const apiRoutes = require('./routes/apiRoutes');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-const db = require('./db/connection');
-const inputCheck = require('./utils/inputCheck');
 
-const apiRoutes = require('./routes/apiRoutes');
 // Express Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// api routes
 app.use('/api', apiRoutes);
 
-
-// app.get('/', (req, res) => {
-//     res.json({
-//         message: 'Hello World'
-//     });
-// });
-
-
-
-
-
+// default response for any other request (not found)
 app.use((req, res) => {
     res.status(404).end();
 });
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// start the server connection
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected');
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 });
